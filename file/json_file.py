a476a1d8ec8776eee0eaf22b41856280b71d2980
1g-v <vasyaigum@gmail.com>
2022-09-23 14:30:44 +0300

    Implemented a submenu for selecting actions

--- file/json_file.py
+++ file/json_file.py
@@ -1,30 +1,114 @@
+from pick import pick
 import json
+import os
 
 
 class Person:
     def __init__(self, d):
         self.__dict__ = d
-
     # def encoder(self):
     #     return {self.__class__.__name__: self.__dict__}
 
 
-# data = {"Object": {"attr": "value", "attr2": {"1st": "1st value", "2nd": "2nd value"}, "attr3": [1, 2, 3]}}
-
-new_data = {}
-while True:
-    key = input("Enter the name of attr: ")
-    if key == "":
-        break
-    value = input("Enter value: ")
-    new_data[key] = value
+def add_data_to_json(data={}):
+    with open("tmp.json", "w") as file:
+        json.dump(data, file, indent=4)
 
 
-user = Person(new_data)
+def merge(obj):
+    try:
+        with open("tmp.json", "r") as file:
+            data = json.load(file) | obj.__dict__
+            return data
+    except Exception as ex:
+        print(ex)
+        input("\n\nPress Enter to continue...")
 
 
-with open("tmp.json", "r") as file:
-    data = json.load(file) | user.__dict__
+def create_file():
+    title = '[Add sample data to a file?]'
+    options = ['Yes', 'No']
+    option, index = pick(options, title, indicator='=>', default_index=0)
+    match index:
+        case 0:
+            sample_data = {"Object": {"attr": "value", "attr2": {"1st": "1st value", "2nd": "2nd value"}, "attr3": [1, 2, 3]}}
+            add_data_to_json(sample_data)
+        case 1:
+            add_data_to_json()
 
-with open("tmp.json", "w") as file:
-    json.dump(data, file, indent=4)
+
+def create_new_object():
+    os.system("cls")
+    new_data = {}
+    print("\nEnter an empty attribute name to stop\n")
+    while True:
+        key = input("Enter the name of attr: ")
+        if key == "":
+            break
+        value = input("Enter value: ")
+        new_data[key] = value
+    new_obj = Person(new_data)
+
+    title = '[Serialize to JSON and add an object to file?]'
+    options = ['Yes', 'No']
+    option, index = pick(options, title, indicator='=>', default_index=0)
+    match index:
+        case 0:
+            try:
+                data = merge(new_obj)
+                print("\n\nData was merged.")
+                add_data_to_json(data)
+            except Exception as ex:
+                os.system("cls")
+                print(ex)
+                input("\n\nPress Enter to continue...")
+        case 1:
+            pass
+
+
+def output_file():
+    os.system("cls")
+    try:
+        with open("tmp.json", "r") as tmp:
+            for line in tmp.read().splitlines():
+                print(line)
+        input("\n\n => Back")
+    except Exception as ex:
+        print(ex)
+        input("\n\nPress Enter to continue...")
+
+
+def submenu():
+    title = '[Work with JSON file]'
+    options = ['Create JSON file', 'Add new JSON lines', 'Output file', 'Delete file', 'Back']
+    option, index = pick(options, title, indicator='=>', default_index=0)
+    return index
+
+
+def delete_file():
+    os.system("cls")
+    try:
+        path = os.path.abspath('tmp.json')
+        os.remove(path)
+    except Exception as ex:
+        print(ex)
+        input("\n\nPress Enter to continue...")
+
+
+def work_with_json():
+    while True:
+        match submenu():
+            case 0:
+                create_file()
+                continue
+            case 1:
+                create_new_object()
+                continue
+            case 2:
+                output_file()
+                continue
+            case 3:
+                delete_file()
+                continue
+            case 4:
+                break
