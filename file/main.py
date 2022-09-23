4d7b966d4e6bb0704c39cae4ddf070d3f07b768f
1g-v <vasyaigum@gmail.com>
2022-09-23 14:29:39 +0300

    Work with JSON has been added to the selection menu

--- file/main.py
+++ file/main.py
@@ -2,11 +2,12 @@ from pick import pick
 import os
 from disk_info import disk_info
 from simple_file import work_with_file
+from json_file import work_with_json
 
 
 def main_menu():
     title = '1st lab | author: Igumenshchev Vasily | github: 1g-v'
-    options = ['View disk info', 'Work with a text file']
+    options = ['View disk info', 'Work with a text file', 'Work with a JSON file']
     option, index = pick(options, title, indicator='=>', default_index=0)
     return index
 
@@ -19,6 +20,8 @@ def main():
                 disk_info()
             case 1:
                 work_with_file()
+            case 2:
+                work_with_json()
             case _:
                 print("err")
 
